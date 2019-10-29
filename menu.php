ba6f6c42e0a038f7c7bf1116d4de5dcb1812c969
jakehein <32526235+jakehein@users.noreply.github.com>
2019-10-29 13:31:18 -0500

    updated shit

--- menu.php
+++ menu.php
@@ -7,6 +7,7 @@
         <meta charset="UTF-8">
         <meta name="author" content="Jacob Hein, Jason Hoffman, Luke Rolf">
         <meta name="description" content="This is a student made website for Jade Dragon.">
+        <meta name="robots" content="noindex, nofollow">
         <meta name="keywords" content="food, chinese, take out, delivery, rice, noodle, chicken, pork, jade, dragon, crab, rangoon, general, tso, Jade Dragon, Oshkosh, WI, 54901, Chinese Menu, Chinese restaurant Oshkosh, Chinese restaurant 54901, Chinese food Oshkosh, Chinese food 54901, Chinese food delivery, Chinese delivery Oshkosh, Chinese delivery 54901, Chinese food catering, Chinese carry out, Chinese dine in, Chinese party trays, Chinese food order online">
         <title>Jade Dragon</title>
         <link rel="icon" href="dragon.jpg"> <!-- #Test Your Might -->
@@ -14,5 +15,7 @@
     </head>
     <body>
         <?php include "heading.html";?>
+        <p>Customer can select items in menu to order if logged in. Radio menu selection (disable if not logged in). Add totals for items selected.</p>
+        <?php include "footer.html";?>
     </body>
 </html>

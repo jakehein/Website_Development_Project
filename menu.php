c5b50545df7758e9dbef691d0f43761c2c93e218
Refridgeratr <Lukeorion0@gmail.com>
2019-12-03 15:50:14 -0600

    Large/small price vs singular price stuff

--- menu.php
+++ menu.php
@@ -23,22 +23,46 @@
             $categories = get_categories();
 
             foreach($categories as $category){
+                $items = get_menu_by_category($category);
+
+                // check if there needs to be separate large/small prices
+                $differentSizes = false;
+                foreach($items as $item){
+                    if ($item[3] > 0){
+                        $differentSizes = true;
+                    }
+                }
         ?>
             <h2> <?= $category ?> </h2>
             <table>
                 <tr>
                     <th>Name</th>
+        <?php
+                    if($differentSizes) {
+        ?>
                     <th>Large Price</th>
                     <th>Small Price</th>
+        <?php
+                    } else {
+        ?>
+                    <th>Price</th>
+        <?php
+                    }
+        ?>
                 </tr>
         <?php
-                $items = get_menu_by_category($category);
                 foreach($items as $item){
         ?>
                 <tr>
                     <td><?= $item[1] ?></td>
                     <td><?= $item[2] ?></td>
+        <?php
+                    if($differentSizes){
+        ?>
                     <td><?= $item[3] ?></td>
+        <?php
+                    }
+        ?>
                 </tr>
         <?php
                 }

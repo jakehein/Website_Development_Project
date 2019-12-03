5ba04e40551113f6a00becd03695263e0346b4e9
jakehein <32526235+jakehein@users.noreply.github.com>
2019-12-03 13:58:23 -0600

    changed file locations of images

---
+++ Website_Development_Project-master/jadeDragon.php
@@ -0,0 +1,23 @@
+<?php
+    error_reporting(E_ALL | E_STRICT);
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Jacob Hein, Jason Hoffman, Luke Rolf">
+        <meta name="description" content="This is a student made website for Jade Dragon.">
+        <meta name="robots" content="noindex, nofollow">
+        <meta name="keywords" content="food, chinese, take out, delivery, rice, noodle, chicken, pork, jade, dragon, crab, rangoon, general, tso, Jade Dragon, Oshkosh, WI, 54901, Chinese Menu, Chinese restaurant Oshkosh, Chinese restaurant 54901, Chinese food Oshkosh, Chinese food 54901, Chinese food delivery, Chinese delivery Oshkosh, Chinese delivery 54901, Chinese food catering, Chinese carry out, Chinese dine in, Chinese party trays, Chinese food order online">
+        <title>Jade Dragon</title>
+        <link rel="icon" href="images/dragon.jpg"> <!-- #Test Your Might -->
+        <link rel="stylesheet" type="text/css" href="jadeDragon.css">
+    </head>
+    <body>
+        <?php include "heading.php";?>
+		<h1>Welcome to Jade Dragon!</h1>
+        <p>provide login for user/manager/owner</p>
+        <p>login can be email for user, "pin" for manager/owner</p>
+        <?php include "footer.html";?>
+    </body>
+</html>

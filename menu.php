b24e796d53aa10b61aeadf3a4d8c9a0a44cc35f9
Refridgeratr <Lukeorion0@gmail.com>
2019-12-05 15:11:02 -0600

    Now it actually adds stuff to the server

--- menu.php
+++ menu.php
@@ -8,7 +8,25 @@
     $owner = false;
     if(isset($_SESSION["status"])){
 		$owner = $_SESSION["status"] == "Owner";
-	}
+    }
+
+    $itemName = "";
+    $largePrice = "";
+    $smallPrice = "";
+    $category = "";
+    
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $itemName = $_POST["itemName"];
+        $largePrice = $_POST["largePrice"];
+        if(isset($_POST["smallPrice"])){
+            $smallPrice = $_POST["smallPrice"];
+        } else {
+            $smallPrice = 0.00;
+        }
+        $category = $_POST["category"];
+    
+        add_menu_item($itemName, $largePrice, $smallPrice, $category);
+    }
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
@@ -30,7 +48,24 @@
             <br>
             <button class="accordion">Add new item: </button>
             <div class="panel">
-            <?php include "menuItemAdd.php"; ?>
+            <h2>Add new menu item:</h2>
+                <form method="POST" id="addItemForm">
+                    <label for="itemName">Item Name:</label><br>
+                    <input type="text" name="itemName" id="itemName" value="<?= $itemName ?>" required><br>
+
+                    <p>Note: If there are no separate large/small sizes, only enter large price</p>
+
+                    <label for="largePrice">Large Price:</label><br>
+                    <input type="text" name="largePrice" id="largePrice" value="<?= $largePrice ?>" required><br>
+
+                    <label for="smallPrice">Small Price:</label><br>
+                    <input type="text" name="smallPrice" id="largePrice" value="<?= $smallPrice ?>"><br>
+
+                    <label for="category">Category:</label><br>
+                    <input type="text" name="category" id="category" value="<?= $category ?>" required><br>
+
+                    <input type="submit" id="submitItem" value="Add Item">
+                </form>
             </div>
             <?php
         }

bbac36401f8905fb04eb190b14b003ff21bc33b3
Refridgeratr <Lukeorion0@gmail.com>
2019-12-08 12:14:05 -0600

    Re-added the item ID on the menu

--- menuBody.php
+++ menuBody.php
@@ -18,6 +18,7 @@
     <div class="panel">
         <table>
             <tr>
+                <th>ID</th>
                 <th>Name</th>
 <?php
                 if($differentSizes) {
@@ -36,6 +37,7 @@
             foreach($items as $item){
 ?>
             <tr class="item">
+                <td class="itemId"><?= $item[0] ?></td>
                 <td class="itemName"><?= $item[1] ?></td>
                 <!-- Attempting to make optional buttons that'll only appear
                      on the orderOnline page-->

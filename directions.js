cad59a5433825085f1450464fdbe04ed00e6dc0b
Jason Hoffman <hoffmj33@uwosh.edu>
2019-12-09 15:37:45 -0600

    fixing html validation moved map to js file

---
+++ directions.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+window.addEventListener("DOMContentLoaded", function () {
+    var map = document.getElementById("map");
+    map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2868.996227205137!2d-88.54903428449026!3d44.02147247911045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8803ebef8aa5829f%3A0xcc9f48d7d15771e6!2s252%20Wisconsin%20St%2C%20Oshkosh%2C%20WI%2054901!5e0!3m2!1sen!2sus!4v1571941600164!5m2!1sen!2sus";
+});

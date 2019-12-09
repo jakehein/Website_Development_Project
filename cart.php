27b69c94e72a08349d5a7b41c2ed72d66ba52505
Jacob Hein <heinj82@uwosh.edu>
2019-12-09 16:50:47 -0600

    still nothing to see

--- cart.php
+++ cart.php
@@ -13,17 +13,3 @@
     <span class="totalPrice">$0</span>
 </div>
 <button class="checkout" type="button">PURCHASE</button>
-
-    <!--<div class="cartRow">
-        <span class="cartItemID cartHeader cartColumn">ID</span>
-        <span class="cartItem cartHeader cartColumn">ITEM</span>
-        <span class="cart-price cartHeader cartColumn">PRICE</span>
-        <span class="cart-quantity cartHeader cartColumn">QUANTITY</span>
-    </div>
-    <div class="cartItems">
-    </div>
-    <div class="cartTotal">
-        <h2>Total: </h2>
-        <span class="totalPrice">$0</span>
-    </div>
-    <button class="checkout" type="button">PURCHASE</button>-->
